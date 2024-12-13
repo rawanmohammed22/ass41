5ff28e27603b032af8021e9751bb989098aa5eda
rawanmohammed22 <rawanmohammed22>
2024-12-13 13:13:23 +0200

    first commit

---
+++ ConsoleApp10/Program.cs
@@ -0,0 +1,73 @@
+﻿using System.Threading.Channels;
+
+namespace ConsoleApp10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello, World!");
+
+            //#region  explict 
+
+            //long x = 10000000000000000000000;
+
+
+            bool flag = int.TryParse(Console.ReadLine(), out int p);
+            Console.WriteLine(flag);
+
+
+            if (flag == true)
+            {
+
+                if (p % 3 == 0 && p % 4 == 0)
+                    Console.WriteLine("yes");
+                else
+                    Console.WriteLine("no");
+
+            }
+
+
+            ////int y = x.GetType() == typeof(int) ? (int)x : 0;
+            ////Console.WriteLine(y);
+            //int o;
+            //string t ="9";
+            //Boolean flag = int.TryParse(t, out o);
+            //Console.WriteLine(flag);
+            //Console.WriteLine(o);
+
+            ////#endregion
+            ////int f = 9;
+            ////++f;
+
+            ////Console.WriteLine(f);
+            ////Console.WriteLine(f++);
+
+
+            ////Console.WriteLine(f);
+
+            ////int d;
+            ////d = Convert.ToInt32(!(30 < 20));  
+
+
+            ////Console.WriteLine(d);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
